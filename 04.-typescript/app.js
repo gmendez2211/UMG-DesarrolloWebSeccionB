9ae1f3a0a0460b5c684ac00e857e9c79fb951ca6
Gustavo Mendez <gmendez.2211@gmail.com>
2023-07-30 22:55:27 -0600

    Primera version

---
+++ 04.-typescript/app.js
@@ -0,0 +1,7 @@
+function fnSaludar1(sNombre) {
+    console.table('Hola ' + sNombre); // Hola Estudiante
+}
+var wolverine = {
+    sNombre: ''
+};
+fnSaludar("Gustavo");
---
+++ 04.-typescript/app.ts
@@ -0,0 +1,14 @@
+
+
+function fnSaludar1( sNombre : string) {
+    console.table( 'Hola ' + sNombre ); // Hola Estudiante
+}
+
+
+const wolverine = {
+    sNombre: ''
+};
+
+
+fnSaludar( "Gustavo" );
+
